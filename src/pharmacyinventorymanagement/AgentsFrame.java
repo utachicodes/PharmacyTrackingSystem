3d55e713c46d34a9d3af42f03ed42875afdf63f3
utachicodes <abdoullahaljersi@gmail.com>
2026-05-19 23:31:14 +0000

    refactor(AgentsFrame): remove unused imports (DriverManager, DriverManager)

--- src/pharmacyinventorymanagement/AgentsFrame.java
+++ src/pharmacyinventorymanagement/AgentsFrame.java
@@ -5,13 +5,12 @@ package pharmacyinventorymanagement;
  * @author Abdoullah Ndao
  */
 
-import java.sql.*;
-import java.sql.Statement;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import javax.swing.JOptionPane;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import javax.swing.*;
