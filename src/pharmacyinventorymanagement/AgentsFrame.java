ddf9ba70dba0331fb251905ec359351531373027
utachicodes <abdoullahaljersi@gmail.com>
2026-05-19 23:30:22 +0000

    ux(AgentsFrame): replace System.exit with confirm-exit dialog on close

--- src/pharmacyinventorymanagement/AgentsFrame.java
+++ src/pharmacyinventorymanagement/AgentsFrame.java
@@ -671,7 +671,8 @@ public class AgentsFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_btnUpdateMouseClicked
 
     private void closeXMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_closeXMouseClicked
-        System.exit(0);
+        int choice = JOptionPane.showConfirmDialog(this, "Exit application?", "Confirm Exit", JOptionPane.YES_NO_OPTION);
+        if (choice == JOptionPane.YES_OPTION) System.exit(0);
     }//GEN-LAST:event_closeXMouseClicked
 
     private void sellerBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_sellerBtnMouseClicked
