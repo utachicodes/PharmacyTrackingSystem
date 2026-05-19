18ffb60ea013fd21e3d375b81dc3218b56e2e70d
utachicodes <abdoullahaljersi@gmail.com>
2026-05-19 23:30:59 +0000

    refactor(LoginFrame): remove unused imports (DriverManager, Statement, etc.)

--- src/pharmacyinventorymanagement/LoginFrame.java
+++ src/pharmacyinventorymanagement/LoginFrame.java
@@ -7,13 +7,11 @@ package pharmacyinventorymanagement;
  * @author Abdoullah Ndao
  */
 
-import java.sql.*;
-import java.sql.Statement;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
-import java.sql.ResultSet;
 import javax.swing.table.DefaultTableModel;
 public class LoginFrame extends javax.swing.JFrame {
 
