abd6bbadbb86d42d0bf6d5e3a2a9f2753c84adcb
utachicodes <abdoullahaljersi@gmail.com>
2026-05-19 23:31:50 +0000

    refactor(SellingFrame): remove dead editor-fold comments in main method

--- src/pharmacyinventorymanagement/SellingFrame.java
+++ src/pharmacyinventorymanagement/SellingFrame.java
@@ -619,16 +619,6 @@ public class SellingFrame extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Create and display the form */
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new SellingFrame().setVisible(true);
